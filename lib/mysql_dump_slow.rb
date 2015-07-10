82c69962fd3a43ac0cc0de05ff2288458403ead7
monochromegane <dev.kuro.obi@gmail.com>
2015-07-10 18:57:51 +0900

    Added module method.

--- lib/mysql_dump_slow.rb
+++ lib/mysql_dump_slow.rb
@@ -4,5 +4,7 @@ require "mysql_dump_slow/masked_sql_parser"
 require "mysql_dump_slow/summary"
 
 module MysqlDumpSlow
-  # Your code goes here...
+  def self.summarize(slow_logs)
+    Summary.new(slow_logs)
+  end
 end
